c5e832a77ffb33c8e9b63fb466b31d81eb767bbd
Rivomanana MANDANIAINA <contact@riv0manana.dev>
2024-11-25 15:13:43 +0300

    [Coupon Listing] [Fix] fix margin

--- components/organisms/CouponListing/CouponListing.tsx
+++ components/organisms/CouponListing/CouponListing.tsx
@@ -12,7 +12,7 @@ const CouponListing = ({
     className,
 }: CouponListingProps) => {
     return (
-        <div className={cn("container !px-0 flex flex-wrap gap-[32px] justify-center", className)}>
+        <div className={cn("container !px-0 flex flex-wrap gap-[32px] justify-center pt-8", className)}>
             {items.map((item, idx) => (
                 <CouponCard isLCP={idx === 0} item={item} key={item.id} />
             ))}
